0a4247c9d7284a97696af6ae593185120e38f6b2
Aasthayuli <aasthayuli2000@gmail.com>
2026-03-03 12:42:31 +0530

    DB config in properties file

--- src/hospital/config/DBConnection.java
+++ src/hospital/config/DBConnection.java
@@ -1,25 +1,46 @@
 package hospital.config;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
-public class DBConnection{
-    private static final String URL = "jdbc:mysql://localhost:3306/HMS";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Mysqlpass26";
+public class DBConnection {
+    private static final Properties props = new Properties();
+
+    static {
+        try (InputStream input = DBConnection.class
+                .getClassLoader()
+                .getResourceAsStream("db.properties")) {
+
+            if (input == null) {
+                throw new RuntimeException("db.properties NOT FOUND in classpath");
+            }
+
+            props.load(input);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load DB properties", e);
+        }
+    }
 
     private static Connection connection;
 
-    private DBConnection(){}
+    private DBConnection() {
+    }
 
-    public static Connection getConnection() throws SQLException{
+    public static Connection getConnection() throws SQLException {
 
-        if(connection == null || connection.isClosed()){
-            try{
+        if (connection == null || connection.isClosed()) {
+            try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
-                connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            }catch(ClassNotFoundException e){
+                connection = DriverManager.getConnection(
+                        props.getProperty("url"),
+                        props.getProperty("user"),
+                        props.getProperty("password"));
+            } catch (ClassNotFoundException e) {
                 throw new RuntimeException("MySQL Driver not found.", e);
             }
         }
